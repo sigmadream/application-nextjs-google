233374b5c96277edf561830eb6c8954e3d11a294
Sangkon, Han <sigmadream@gmail.com>
2023-06-15 02:44:58 +0900

    feat: Step-1 추가
    - Add home and header components

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -6,7 +6,7 @@ export default function Head() {
       <title>Google Clone Next.js v13</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Google Clone Next.js v13" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.svg" />
     </>
   );
 }
