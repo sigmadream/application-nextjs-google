6e4b0d278f205651461fabdece30a12ae6c8e187
Sangkon Han <sigmadream@gmail.com>
2023-07-24 02:23:01 +0900

    feat: Step-5 추가
    Add Web and Image Search

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,14 @@
 import Footer from "@/components/Footer";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import SearchHeader from "@/components/SearchHeader";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className + " relative min-h-screen"}>
         {children}
         <Footer />
       </body>
